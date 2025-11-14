721e65e682c9b17d0628169f7afba293068f1d1d
Rachel Kudzin <heyrachel221@gmail.com>
2025-11-14 13:34:27 -0500

    Reference requirments.txt from .sh file so that we don't get redundancy

--- setup.sh
+++ setup.sh
@@ -19,35 +19,7 @@ conda run --prefix ./env pip install torch torchvision torchaudio --index-url ht
 
 echo "--- Installing all other packages ---"
 # NOTE: The indentation here is with standard spaces, which will work correctly.
-conda run --prefix ./env pip install \
-    ipykernel \
-    redis==5.2.1 \
-    jupyter==1.1.1 \
-    numpy==2.1.2 \
-    pandas==2.3.0 \
-    matplotlib==3.10.1 \
-    scipy==1.15.2 \
-    scikit-learn==1.6.1 \
-    tqdm==4.67.1 \
-    g2p_en==2.1.0 \
-    h5py==3.13.0 \
-    omegaconf==2.3.0 \
-    editdistance==0.8.1 \
-    huggingface-hub==0.33.1 \
-    transformers==4.53.0 \
-    tokenizers==0.21.2 \
-    accelerate==1.8.1 \
-    bitsandbytes==0.46.0 \
-    unidecode \
-    levenshtein \
-    torch \
-    datasets \
-    transformers[torch] \
-    evaluate \
-    nltk \
-    rouge_score \
-    sentencepiece \
-    braindecode
+conda run --prefix ./env pip install -r requirements.txt
 
 echo
 echo "--- Setup Complete! ---"

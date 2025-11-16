e84d14470e076d488af87ffd24c3b11a2578ab34
Sean2418 <sean20030418@gmail.com>
2025-11-16 13:42:55 -0500

    Updated setup.py for language model

--- language_model/runtime/server/x86/setup.py
+++ language_model/runtime/server/x86/setup.py
@@ -47,6 +47,7 @@ class CMakeBuild(build_ext):
             f"-DPYTHON_EXECUTABLE={sys.executable}",
             f"-DCMAKE_BUILD_TYPE={cfg}",  # not used on MSVC, but no harm
             "-DCMAKE_POLICY_VERSION_MINIMUM=3.5",
+            "-DCMAKE_FIND_ROOT_PATH_MODE_PACKAGE=ONLY",
         ]
         build_args = []
         # Adding CMake arguments set as environment variable

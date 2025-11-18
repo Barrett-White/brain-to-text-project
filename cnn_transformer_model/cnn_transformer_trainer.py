46471a0cc4ea16f6df33edae6d035a38066ba2d5
Rachel Kudzin <heyrachel221@gmail.com>
2025-11-18 14:42:06 -0500

    Add printed info about which layers are frozen

--- cnn_transformer_model/cnn_transformer_trainer.py
+++ cnn_transformer_model/cnn_transformer_trainer.py
@@ -254,6 +254,11 @@ class CNN_Transformer_Trainer:
                 if "lm_head" not in name:
                     param.requires_grad = False
 
+        # Print out frozen info
+        for name, param in self.model.named_parameters():
+            if not param.requires_grad:
+                print(f"Frozen: {name}")
+
         self.model.to(self.device)
 
     def create_optimizer(self):
